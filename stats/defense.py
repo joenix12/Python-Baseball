1d91c3a02b351cfa82c28042f90391dea93e6ffb
Cheng Qiao <chengq@microsoft.com>
2019-09-21 16:52:33 +0800

    Update defense.py

--- stats/defense.py
+++ stats/defense.py
@@ -19,12 +19,12 @@ events_plus_pa = pd.merge(events, pa, how='outer', left_on=['year', 'game_id', '
 
 defense = pd.merge(events_plus_pa, info)
 
-defense = defense.loc[:, 'DER'] = 1 - ((defense['H'] + defense['ROE']) /
+defense.loc[:, 'DER'] = 1 - ((defense['H'] + defense['ROE']) /
 (defense['PA'] - defense['BB'] - defense['SO'] - defense['HBP'] - defense['HR']))
 
-defense.loc[:, 'year'] = pd.to_numeric(defense.loc['year'])
+defense.loc[:, 'year'] = pd.to_numeric(defense['year'])
 der = defense.loc[defense['year'] >= 1978, ['year', 'defense', 'DER']]
 der = der.pivot(index='year', columns='defense', values='DER')
 
-der.plot(x_compat=True, xticks=Range(1978, 2018, 4), rot=45)
+der.plot(x_compat=True, xticks=range(1978, 2018, 4), rot=45)
 plt.show()
