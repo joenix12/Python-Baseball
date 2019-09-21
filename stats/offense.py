cd86c59a31aac9d86392fd32f0ef956540e4b581
Cheng Qiao <chengq@microsoft.com>
2019-09-21 15:28:47 +0800

    Update offense.py

--- stats/offense.py
+++ stats/offense.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from data import games
 
 plays = games[games['type'] == 'play']
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
 hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
-
 hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
-replacements = {
-r'^S(.*)': 'single',
-r'^D(.*)': 'double',
-r'^T(.*)': 'triple',
-r'^HR(.*)': 'hr'}
-
+replacements = {r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple', r'^HR(.*)': 'hr'}
 hit_type = hits['event'].replace(replacements, regex=True)
 hits = hits.assign(hit_type=hit_type)
-hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
+
+hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
 
 hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
 
