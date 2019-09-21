78920a4d583621dde069ab6d437c9ad82153d9ad
Cheng Qiao <chengq@microsoft.com>
2019-09-21 12:50:44 +0800

    Update attendance.py

---
+++ stats/attendance.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from data import games
+
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+attendance.columns = ['year', 'attendance']
+
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
+
+attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+
+plt.xlabel('year')
+plt.ylable('Attendance')
+plt.axhline(y = attendance['attendance'].mean(), ylable='mean', linestyle='--', color = 'green')
+plt.show()
